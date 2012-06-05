5983a6ead78979d7d67c82408b404a55b42ac33e
Dmitry Malchikov <dmalch@gmail.com>
2012-06-05 21:40:59 +0400

    Version 2.0. Fill background in all tree views

---
+++ src/com/github/dmalch/ColorIdeApplicationComponent.java
@@ -0,0 +1,27 @@
+package com.github.dmalch;
+
+import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class ColorIdeApplicationComponent implements ApplicationComponent {
+    public ColorIdeApplicationComponent() {
+    }
+
+    public void initComponent() {
+        final EditorColorsScheme globalScheme = EditorColorsManager.getInstance().getGlobalScheme();
+        UIManager.put("Viewport.foreground", globalScheme.getDefaultForeground());
+        UIManager.put("Tree.background", globalScheme.getDefaultBackground());
+    }
+
+    public void disposeComponent() {
+    }
+
+    @NotNull
+    public String getComponentName() {
+        return "Applc";
+    }
+}

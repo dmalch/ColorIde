0fa7d1bcb764c30e8d5067124850918e1087fdd0
Dmitry Malchikov <dmalch@gmail.com>
2012-07-06 18:26:03 +0400

    Added settings menu

--- src/main/java/com/github/dmalch/ColorIdeSystemSettings.java
+++ src/main/java/com/github/dmalch/ColorIdeSystemSettings.java
@@ -1,15 +1,26 @@
 package com.github.dmalch;
 
+import com.google.common.base.Objects;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.util.ui.ThreeStateCheckBox;
 import org.jetbrains.annotations.Nls;
 
 import javax.swing.*;
 
+import static com.github.dmalch.ColorIdeApplicationComponent.USER_ACCEPTED_PATCHING;
+import static com.intellij.util.ui.ThreeStateCheckBox.State.*;
+
 public class ColorIdeSystemSettings implements Configurable {
     private JPanel myPanel;
     private JLabel headerText;
-    private JCheckBox patchIdea;
+    private ThreeStateCheckBox shouldPatchIdea;
+
+    private final PersistenceManager persistenceManager = new PersistenceManagerImpl();
+
+    private final ColorIdePatcher patcher = new ColorIdePatcherImpl();
+
+    private ThreeStateCheckBox.State initialState;
 
     @Nls
     @Override
@@ -27,9 +38,17 @@ public class ColorIdeSystemSettings implements Configurable {
         return myPanel;
     }
 
+    private boolean isUserHasAcceptedPatching() {
+        return persistenceManager.getBoolean(USER_ACCEPTED_PATCHING, true);
+    }
+
+    private boolean filesArePatched() {
+        return patcher.checkFilesArePatched();
+    }
+
     @Override
     public boolean isModified() {
-        return false;
+        return Objects.equal(shouldPatchIdea.getState(), initialState);
     }
 
     @Override
@@ -38,6 +57,18 @@ public class ColorIdeSystemSettings implements Configurable {
 
     @Override
     public void reset() {
+        final boolean userHasAcceptedPatching = isUserHasAcceptedPatching();
+        final boolean filesArePatched = filesArePatched();
+
+        if (userHasAcceptedPatching && filesArePatched) {
+            initialState = SELECTED;
+        } else if (userHasAcceptedPatching || filesArePatched) {
+            initialState = DONT_CARE;
+        } else {
+            initialState = NOT_SELECTED;
+        }
+
+        shouldPatchIdea.setState(initialState);
     }
 
     @Override
